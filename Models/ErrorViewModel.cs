738a4803f0f190c005534720c3654656beada358
Santiago Ramirez <sr10973@unphu.edu.do>
2024-09-14 10:39:22 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationPrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
